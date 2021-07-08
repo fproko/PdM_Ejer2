ac4a6ad60c81227d8271be093f7ee7e315433d64
fproko <83043274+fproko@users.noreply.github.com>
2021-07-07 21:19:51 -0300

    Commit Inicial

---
+++ inc/main.h
@@ -0,0 +1,40 @@
+/*=============================================================================
+ * Author: Fernando Prokopiuk <fernandoprokopiuk@gmail.com>
+ * Date: 2021/07/07
+ *===========================================================================*/
+
+/*=====[Avoid multiple inclusion - begin]====================================*/
+
+#ifndef __PDM_EJER2_H__
+#define __PDM_EJER2_H__
+
+/*=====[Inclusions of public function dependencies]==========================*/
+
+#include <stdint.h>
+#include <stddef.h>
+
+/*=====[C++ - begin]=========================================================*/
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*=====[Definition macros of public constants]===============================*/
+
+/*=====[Public function-like macros]=========================================*/
+
+/*=====[Definitions of public data types]====================================*/
+
+/*=====[Prototypes (declarations) of public functions]=======================*/
+
+/*=====[Prototypes (declarations) of public interrupt functions]=============*/
+
+/*=====[C++ - end]===========================================================*/
+
+#ifdef __cplusplus
+}
+#endif
+
+/*=====[Avoid multiple inclusion - end]======================================*/
+
+#endif /* __PDM_EJER2_H__ */
